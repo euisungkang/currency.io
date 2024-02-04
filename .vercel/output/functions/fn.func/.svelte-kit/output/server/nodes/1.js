0a3220968d570da70bc6ec796307944df45d8bf8
Easton Kang <euisungkang@gmail.com>
2024-02-04 16:37:01 +0900

    vercel settings

---
+++ .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
@@ -0,0 +1,8 @@
+
+
+export const index = 1;
+let component_cache;
+export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
+export const imports = ["_app/immutable/nodes/1.IAgcSmAR.js","_app/immutable/chunks/scheduler.SvYxWDf0.js","_app/immutable/chunks/index.tReDjvA9.js","_app/immutable/chunks/entry._7UcNvIT.js","_app/immutable/chunks/index.62iC4oRv.js"];
+export const stylesheets = [];
+export const fonts = [];
