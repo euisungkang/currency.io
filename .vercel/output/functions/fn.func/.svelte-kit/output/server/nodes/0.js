f902693bc99d65917ea5d6d255c8521e4a65359f
Easton Kang <euisungkang@gmail.com>
2024-02-04 17:19:07 +0900

    vercel redeploy

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.Z200pxoC.js","_app/immutable/chunks/scheduler.SvYxWDf0.js","_app/immutable/chunks/index.tReDjvA9.js"];
-export const stylesheets = ["_app/immutable/assets/0.rtzfn8T5.css"];
-export const fonts = [];
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -13,7 +13,9 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+            reuntime: 'nodejs18.x',
+        }),
 	}
 };
 export default config;
