961405b7734902a024c16d1b1fe70306e9045aa9
Easton Kang <euisungkang@gmail.com>
2024-02-18 18:43:45 +0900

    Add Vercel Analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
 	import '../app.postcss';
+    import { inject } from '@vercel/analytics';
+    import { dev } from '$app/environment';
+
+    inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <slot />
