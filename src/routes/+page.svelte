5bc6c1defa807200d0e9aeafbf59efea8e3ce332
Easton Kang <euisungkang@gmail.com>
2024-02-18 17:25:07 +0900

    UI Update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
-<script>
-    import { LightSwitch } from '@skeletonlabs/skeleton';
+<script lang="ts">
+    import { LightSwitch, tableMapperValues, Table } from '@skeletonlabs/skeleton';
+    import type { TableSource } from '@skeletonlabs/skeleton';
     import Icon from '@iconify/svelte';
     import { parseExchangeKR, parseExchangeEN, separateWithComma } from '$lib/utils/parser';
+    import { Us, Kr, Eu } from 'svelte-flag-icons';
 
     export let data;
     const metadata = data.props.metadata
@@ -9,12 +11,15 @@
 
     let from = "USD";
     let to = "KRW";
-    let amount = 1;
+    let amount;
     let exchange = 0;
     let useCustomRate;
     let rate = (ratedata[to] / ratedata[from]);
     let displayEN = [];
     let displayKR = [];
+
+    let date = new Date();
+    let dateDisplay = date.getFullYear() + "." + date.getMonth() + "." + date.getDate();
 
     function updateExchange() {
         exchange = (amount * rate).toFixed(metadata[to].decimal_digits);
@@ -39,6 +44,29 @@
             updateExchange();
         }
     }
+
+    function selectAll(event) {
+        event.target.select();
+    }
+
+    const flagMap = {
+        "USD": Us,
+        "EUR": Eu,
+        "KRW": Kr
+    };
+
+    const rateTableData = [];
+
+    let position = 1;
+    for (const [k, v] of Object.entries(metadata)) {
+        rateTableData.push({ position: position++, code: k, symbol: v.symbol, rate: ratedata[k].toFixed(2) }); 
+    }
+
+    const rateTable: TableSource = {
+        head: ['Code', 'Symbol', 'Rate'],
+        body: tableMapperValues(rateTableData, ['code', 'symbol', 'rate']),
+        meta: tableMapperValues(rateTableData, ['position', 'code', 'symbol', 'rate']),
+    }
 </script>
 
 <head>
@@ -47,92 +75,103 @@
 </head>
 
 <form method="POST" action="?/convert" class="">
-<div class="h-screen grid grid-rows-1fr-auto-auto max-w-[%75] container mx-auto p-4">
-    <div class="">
-        <LightSwitch />
-        <div class="flex items-center justify-center gap-4">
+<div class="h-screen grid grid-rows-1fr-auto-auto max-w-[%75] container mx-auto p-12">
+    <LightSwitch class="absolute"/>
 
-            <!-- From Field -->
-            <label class="w-1/2 flex-col items-center justify-center">
-                <span class="">From</span>
-                <select bind:value={from}
-                        on:change={updateRates}
-                        on:change={updateExchange}
-                        class="select">
-                        <!-- size="3"> -->
-                    <option value="USD">USD</option>
-                    <option value="KRW">KRW</option>
-                    <option value="EUR">EUR</option>
-                </select>
-            </label>
+        <div class="grid grid-cols-3 space-x-8">
 
-            <!-- Switch Button -->
-            <label class="flex-col items-center justify-center">
-                <span class="">&nbsp</span>
-                <button on:click={switchCurrency}
-                        type="button"
-                        class="btn-icon variant-filled">
-                        <Icon icon="mi:switch" />
-                </button>
-            </label>
-
-            <!-- To Field -->
-            <label class="w-1/2 flex-col items-center justify-center">
-                <span class="">To</span>
-                <select bind:value={to}
-                        on:change={updateRates}
-                        on:change={updateExchange}
-                        class="select">
-                        <!-- size="3"> -->
-                    <option value="USD">USD</option>
-                    <option value="KRW">KRW</option>
-                    <option value="EUR">EUR</option>
-                </select>
-            </label>
-        </div>        
-
-        <div class="flex items-center justify-center gap-4">
-
-            <!-- Currency Rate Field -->
-            <label class="w-full flex-col items-center justify-center">
-                <span class="">Exchange Rate</span>
-                <div class="flex items-center justify-center gap-4">
-                    <input bind:value={rate}
-                           disabled={!useCustomRate}
-                           on:input={updateExchange}
-                           type="number"
-                           placeholder="Amount"
-                           class="input" />
-
-                    <!-- Checkbox Daily Currency Rate -->
-                    <label class="flex items-center justify-center text-nowrap">
-                        <input on:click={toggleCustomRate}
-                               type="checkbox"
-                               checked={!useCustomRate}
-                               class="checkbox" />
-                        <span class="ml-2">Use Daily Rate</span>
-                    </label>
+            <div class="flex flex-col items-center justify-center gap-4">
+                <div class="flex justify-center  items-center w-full">
+                    <h1 class="text-xl">Base Currency: <b>USD<b/></h1>
+                    <svelte:component this={flagMap["USD"]} class="ml-2 w-10 h-10" />
                 </div>
-            </label>
-        </div>
+                <Table source={rateTable} class="w-full" />
+                <p class="text-sm text-primary-800">*exchange rates last updated on {dateDisplay}</p>
+            </div>
 
-        <div class="flex-col items-center justify-center">
-            <!-- Amount Field -->
-            <span class="">Exchange Rate</span>
-            <div class="w-full input-group input-group-divider grid-cols-[auto_1fr_auto]">
-                <div>{metadata[from].symbol}</div>
-                    <input type="number" bind:value={amount} on:input={updateExchange} placeholder="Amount" />
+
+            <div class="grid grid-rows-1fr-auto">
+                <div class="flex flex-col items-center justify-end p-4">
+                    <div class="card p-6 flex justify-between items-center w-full relative z-10">
+                        <svelte:component this={flagMap[from]} class="w-10 h-10" />
+                        <select bind:value={from}
+                                on:change={updateRates}
+                                on:change={updateExchange}
+                                class="select place-self-end w-1/3 ">
+                            <option value="USD">USD</option>
+                            <option value="KRW">KRW</option>
+                            <option value="EUR">EUR</option>
+                        </select>
+                    </div>
+
+                    <button on:click={switchCurrency}
+                            type="button"
+                            class="btn-icon variant-filled-surface mt-[-1rem] mb-[-1rem] z-20">
+                            <Icon icon="heroicons-outline:switch-vertical" class="w-7 h-7" />
+                    </button>
+
+                    <div class="card p-6 flex justify-between items-center w-full relative z-10">
+                        <svelte:component this={flagMap[to]} class="w-10 h-10" />
+                        <select bind:value={to}
+                                on:change={updateRates}
+                                on:change={updateExchange}
+                                class="select place-self-end w-1/3">
+                            <option value="USD">USD</option>
+                            <option value="KRW">KRW</option>
+                            <option value="EUR">EUR</option>
+                        </select>
+                    </div>
+                </div>
+
+                <!-- Exchange Rate Field --> 
+                <label class="flex flex-col items-center justify-start">
+                    <div class="flex items-center justify-center gap-4">
+                        <div class="flex flex-col items-center">
+                            <span>Exchange Rate</span>
+                            <input bind:value={rate}
+                                   on:focus={selectAll}
+                                   disabled={!useCustomRate}
+                                   on:input={updateExchange}
+                                   type="number"
+                                   placeholder="Amount"
+                                   class="input" />
+                        </div>
+                        <!-- Checkbox Daily Currency Rate -->
+                        <div class="flex flex-col items-center">
+                            <span>&nbsp</span>
+                            <label class="flex items-center justify-center text-nowrap">
+                                <input on:click={toggleCustomRate}
+                                       type="checkbox"
+                                       bind:checked={useCustomRate}
+                                       class="chip {useCustomRate ? 'variant-filled' : 'variant-soft'} w-5 h-5" />
+                                <span class="ml-2">Use Custom Rate</span>
+                            </label>
+                        </div>
+                    </div>
+                </label>
+            </div>
+
+            <!-- Amount Field --> 
+            <div class="flex flex-col items-center justify-center">
+                <!-- <span class="">Amount</span> -->
+                <div class="w-full input-group input-group-divider grid-cols-[auto_1fr_auto]">
+                    <div>{metadata[from].symbol}</div>
+                        <input type="number" bind:value={amount} on:input={updateExchange} placeholder="Amount" />
+                </div>
             </div>
         </div>
-    </div>
+
 
     <!-- Display Results -->
-    <div class="flex flex-col items-center justify-center">
+    <div class="flex flex-col items-center justify-start">
         {#if !isNaN(amount) && amount > 0}
-
-            <p class="text-center text-2xl">{metadata[to].symbol + " " + separateWithComma(exchange)}</p>
-            <p class="text-center mt-6">{displayEN + metadata[to].code}</p>
-            <p class="text-center">{displayKR + " " +  metadata[to].code}</p>
+            <p class="typed text-center text-3xl">{metadata[to].symbol + " " + separateWithComma(exchange)}</p>
+            <p class="text-center text-xl mt-6">{displayEN + metadata[to].code}</p>
+            <p class="text-center text-xl">{displayKR + " " +  metadata[to].code}</p>
+        {:else}
+            <p class="typed text-center text-3xl">&nbsp</p>
+            <p class="text-center text-xl mt-6">&nbsp</p>
+            <p class="text-center text-xl">&nbsp</p>
         {/if}
     </div>
 
@@ -141,3 +180,18 @@
     </div>
 </div>
 </form>
+
+<style>
+    .typed{
+        overflow: hidden;
+        border-right: .05em solid theme('colors.primary.800');
+        white-space: nowrap;
+        animation:
+            blink 1.5s infinite;
+    }
+    @keyframes blink {
+        from { border-right-color: theme('colors.primary.800'); }
+        60% { border-right-color: transparent; }
+        to { border-right-color: theme('colors.primary.800'); }
+    }
+</style>
