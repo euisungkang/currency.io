0b2b574884b806f54bd726a26407cce2a7961481
Easton Kang <euisungkang@gmail.com>
2024-02-20 11:12:23 +0900

    test: vercel serverless cron job functions

--- src/routes/api/refresh.js
+++ src/routes/api/refresh.js
@@ -3,3 +3,23 @@ export async function get() {
     console.log("Called");
     return new Response("Hello");
 }
+//import fs from 'fs';
+//import { CURRENCY_KEY } from "$env/static/private";
+//import Freecurrencyapi from '@everapi/freecurrencyapi-js';
+//const freecurrencyapi = new Freecurrencyapi(CURRENCY_KEY);
+//
+// export function getCurrencyRatesCache() {
+//     return fs.readFileSync('../data/rate_data.json', 'utf8', (err, data) => {
+//         if (err) {
+//             console.error('Error reading file:', err);
+//             return;
+//         }
+//         try {
+//             const jsonObject = JSON.parse(data);
+//             return jsonObject
+//         } catch (parseError) {
+//             console.error('Error parsing JSON:', parseError);
+//         }
+//     });
+// }
+//
